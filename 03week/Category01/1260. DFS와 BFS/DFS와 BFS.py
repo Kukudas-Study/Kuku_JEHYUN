6d60ceca4003e909abb16a6395722dd5fb663b67
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-17 09:25:25 +0900

    solve : [BOJ] DFS와 BFS_240713

--- 03week/Category01/1260. DFS와 BFS/DFS와 BFS.py
+++ 03week/Category01/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,40 +1,57 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-V, E, S = map(int, input().split())
-node = [[] for _ in range(V+1)]
-visited_d = [0] * (V+1)
-visited_b = [0] * (V+1)
-stk = []
-for _ in range(E):
-    u, v = map(int, input().split())
-    node[u].append(v)
-    node[v].append(u)
-for lst in node:
-    lst.sort()
-n_d = n_b = 1
-def dfs(S):
-    global n_d
-    visited_d[S] = n_d
-    print(S, end=' ')
-    for i in node[S]:
-        if not visited_d[i]:
-            n_d += 1
-            visited_d[i] = n_d
-            dfs(i)
-dfs(S)
-print()
-def bfs(S):
-    global n_b
-    visited_b[S] = n_b
-    stk.append(S)
-    print(S, end=' ')
-    while stk:
-        S = stk.pop(0)
-        for i in node[S]:
-            if not visited_b[i]:
-                n_b += 1
-                stk.append(i)
-                print(i, end = ' ')
-                visited_b[i] = n_b
-bfs(S)
+def dfs(start):
+    # 방명록 찍기
+    visited[start] = True
+    rlt.append(start)
+    # 해당 노드에서 갈 수 있는 위치로 이동
+    for node in nodes[start]:
+        # 방문한적 있다면 패스
+        if visited[node]:
+            continue
+        # 재귀
+        dfs(node)
+
+def bfs(start):
+    # 첫 노드 방문 체크
+    visited[start] = True
+    # 노드 담을 리스트
+    q = deque([])
+    # 시작을 담고
+    q.append(start)
+    # 더이상 갈 곳이 없을 때 까지
+    while q:
+        # 하나씩 뽑아서 탐색
+        s = q.popleft()
+        # 순서대로 담기
+        rlt.append(s)
+        for node in nodes[s]:
+            # 방문했다면 패스
+            if visited[node]:
+                continue
+            q.append(node)
+            # 방문 체크
+            visited[node] = True
+
+
+n, m, v = map(int, input().split())
+nodes = [[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    nodes[a].append(b)
+    nodes[b].append(a)
+    
+# 오름차순 배열
+for i in range(1, n+1):
+    nodes[i].sort()
+
+visited = [False] * (n+1)
+rlt = []
+dfs(v)
+print(*rlt)
+visited = [False] * (n+1)
+rlt = []
+bfs(v)
+print(*rlt)
